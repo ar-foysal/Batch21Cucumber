6e3efed4295ba53d079f7094b1ebaf821f4a1f4a
arfoysal <arfoysal.me@gmail.com>
2024-02-23 22:07:10 +0600

    Add Parallel run and CLI run Commend

--- src/test/java/tesetrunners/CucumberTestRunner.java
+++ src/test/java/tesetrunners/CucumberTestRunner.java
@@ -2,6 +2,7 @@ package tesetrunners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/java/features",
@@ -10,5 +11,11 @@ import io.cucumber.testng.CucumberOptions;
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
         "timeline:test-output-thread/"})
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
 
 }
